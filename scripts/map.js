7b50a81ebdd3e27b4ae665c749a8a5fa73ccc833
FlowCAD <florian.cadoz@gmail.com>
2017-05-10 00:01:40 +0200

    Update map.js
    Firebase auth UI updated.

--- scripts/map.js
+++ scripts/map.js
@@ -64,9 +64,6 @@ var zonesLargesTLSJson = L.geoJson(
         }
     }
 );
-//--------------------------------------------------------------------------------------------//
-//--------------------------------------BASICS FUNCTIONS--------------------------------------//
-
 
 //--------------------------------------------------------------------------------------------//
 //-------------------------------------MAP INITIALIZATION-------------------------------------//
@@ -187,14 +184,14 @@ onMapClickAlertCoord = function () {
 
 onclickPopupContainer.html('<div class="btn-group-vertical" role="group"><button id="onMapClickButton1" type="submit" class="btn btn-primary btn-block" onclick="onMapClickSendPositionMail()"><i class="fa fa-envelope fa-fw" aria-hidden="true"></i> Envoyer cette position par mail</button><button id="onMapClickButton2" type="submit" class="btn btn-primary btn-block" onclick="onMapClickPlaceMarker()"><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i> Placer un marker temporaire</button><button id="onMapClickButton3" type="submit" class="btn btn-primary btn-block" onclick="onMapClickAlertCoord()"><i class="fa fa-globe fa-fw" aria-hidden="true"></i> Récupérer les coordonnées</button></div>');
 
-function onMapClick(e) {
+
+mymap.on('click', function (e) {
     mapClickEvent = e;
     L.popup()
         .setLatLng(e.latlng)
         .setContent(onclickPopupContainer[0])
         .openOn(mymap);
-}
-mymap.on('click', onMapClick);
+});
 //--------------------------------------------------------------------------------------------//
 //------------------------------------------SANDBOX-------------------------------------------//
 //Go searching for openData from Toulouse Metropole
@@ -329,159 +326,127 @@ myXHRSender(openDataSubwayStations);
 
 
 //Login with firebase
-    /**
-     * Handles the sign in button press.
-     */
 function toggleSignIn() {
     if (firebase.auth().currentUser) {
-    // [START signout]
-    firebase.auth().signOut();
-    // [END signout]
+        //If signed in, signout
+        firebase.auth().signOut();
     } else {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    if (email.length < 4) {
-        alert('Please enter an email address.');
-        return;
-    }
-    if (password.length < 4) {
-        alert('Please enter a password.');
-        return;
-    }
-    // Sign in with email and pass.
-    // [START authwithemail]
-    firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode === 'auth/wrong-password') {
-        alert('Wrong password.');
-        } else {
-        alert(errorMessage);
+        var email = document.getElementById('emailForLogin').value,
+            password = document.getElementById('password').value;
+        if (email.length < 4) {
+            alert('Saisir une adresse mail valide.');
+            return;
         }
-        console.log(error);
-        document.getElementById('quickstart-sign-in').disabled = false;
-        // [END_EXCLUDE]
-    });
-    // [END authwithemail]
+        if (password.length < 4) {
+            alert('Mot de passe invalide.');
+            return;
+        }
+        // Sign in with email and password.
+        firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
+            var errorCode = error.code,
+                errorMessage = error.message;
+            if (errorCode === 'auth/wrong-password') {
+                alert('Mauvais mot de passe.');
+            } else if (errorCode === 'auth/user-not-found') {
+                alert('Utilisateur non reconnu, veuillez créer un compte.');
+            } else {
+                alert(errorMessage);
+            }
+            console.log("error :", error);
+            document.getElementById('quickstart-sign-in').disabled = false;
+        });
     }
     document.getElementById('quickstart-sign-in').disabled = true;
 }
-/**
-    * Handles the sign up button press.
-    */
+
 function handleSignUp() {
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
+    var email = document.getElementById('emailForLogin').value,
+        password = document.getElementById('password').value;
     if (email.length < 4) {
-    alert('Please enter an email address.');
-    return;
+        alert('Saisir une adresse mail valide.');
+        return;
     }
     if (password.length < 4) {
-    alert('Please enter a password.');
-    return;
+        alert('Mot de passe invalide.');
+        return;
     }
-    // Sign in with email and pass.
-    // [START createwithemail]
-    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode == 'auth/weak-password') {
-        alert('The password is too weak.');
-    } else {
-        alert(errorMessage);
-    }
-    console.log(error);
-    // [END_EXCLUDE]
+    // Sign up with email and pass.
+    firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
+        var errorCode = error.code,
+            errorMessage = error.message;
+        if (errorCode === 'auth/weak-password') {
+            alert('Le mot de passe est trop faible.');
+        } else if (errorCode === 'auth/email-already-in-use') {
+            alert('Cette adresse mail est déjà utilisée.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
     });
-    // [END createwithemail]
 }
-/**
-    * Sends an email verification to the user.
-    */
+
 function sendEmailVerification() {
-    // [START sendemailverification]
-    firebase.auth().currentUser.sendEmailVerification().then(function() {
-    // Email Verification sent!
-    // [START_EXCLUDE]
-    alert('Email Verification Sent!');
-    // [END_EXCLUDE]
+    firebase.auth().currentUser.sendEmailVerification().then(function () {
+        alert('Email de vérification envoyé !');
     });
-    // [END sendemailverification]
 }
+
 function sendPasswordReset() {
-    var email = document.getElementById('email').value;
-    // [START sendpasswordemail]
-    firebase.auth().sendPasswordResetEmail(email).then(function() {
-    // Password Reset Email Sent!
-    // [START_EXCLUDE]
-    alert('Password Reset Email Sent!');
-    // [END_EXCLUDE]
-    }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // [START_EXCLUDE]
-    if (errorCode == 'auth/invalid-email') {
-        alert(errorMessage);
-    } else if (errorCode == 'auth/user-not-found') {
-        alert(errorMessage);
-    }
-    console.log(error);
-    // [END_EXCLUDE]
+    var email = document.getElementById('emailForLogin').value;
+    firebase.auth().sendPasswordResetEmail(email).then(function () {
+        alert('Email de réinitialisation du mot de passe envoyé!');
+    }).catch(function (error) {
+        var errorCode = error.code,
+            errorMessage = error.message;
+        if (errorCode === 'auth/invalid-email') {
+            alert('Adresse mail invalide.');
+        } else if (errorCode === 'auth/user-not-found') {
+            alert('Utilisateur introuvable.');
+        } else {
+            alert(errorMessage);
+        }
+        console.log(error);
     });
-    // [END sendpasswordemail];
 }
-/**
-    * initApp handles setting up UI event listeners and registering Firebase auth listeners:
-    *  - firebase.auth().onAuthStateChanged: This listener is called when the user is signed in or
-    *    out, and that is where we update the UI.
-    */
+
 function initApp() {
     // Listening for auth state changes.
-    // [START authstatelistener]
-    firebase.auth().onAuthStateChanged(function(user) {
-    // [START_EXCLUDE silent]
-    document.getElementById('quickstart-verify-email').disabled = true;
-    // [END_EXCLUDE]
-    if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        // [START_EXCLUDE]
-        document.getElementById('quickstart-sign-in-status').textContent = 'Signed in';
-        document.getElementById('quickstart-sign-in').textContent = 'Sign out';
-        document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-        if (!emailVerified) {
-        document.getElementById('quickstart-verify-email').disabled = false;
+    firebase.auth().onAuthStateChanged(function (user) {
+        document.getElementById('quickstart-verify-email').disabled = true;
+        if (user) {
+            // User is signed in.
+            var displayName = user.displayName,
+                email = user.email,
+                emailVerified = user.emailVerified,
+                photoURL = user.photoURL,
+                isAnonymous = user.isAnonymous,
+                uid = user.uid,
+                providerData = user.providerData,
+                myUser = null;
+            console.log("Identifiant :", uid, "\n Utilisateur :", displayName, "\n Email : ", email, "\n Email vérifié : ", emailVerified, "\n Photo : ", photoURL, "\n Anonyme : ", isAnonymous, "\n providerData : ", providerData);
+            if (user.displayName === null) {
+                myUser = user.email;
+            } else {
+                myUser = user.displayName;
+            }
+            document.getElementById('quickstart-sign-in-status').textContent = 'Connecté avec ' + myUser;
+            document.getElementById('quickstart-sign-in').textContent = 'Se déconnecter';
+            if (!emailVerified) {
+                document.getElementById('quickstart-verify-email').disabled = false;
+            }
+        } else {
+            // User is signed out.
+            document.getElementById('quickstart-sign-in-status').textContent = 'déconnecté';
+            document.getElementById('quickstart-sign-in').textContent = 'Se connecter';
         }
-        // [END_EXCLUDE]
-    } else {
-        // User is signed out.
-        // [START_EXCLUDE]
-        document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-        document.getElementById('quickstart-sign-in').textContent = 'Sign in';
-        document.getElementById('quickstart-account-details').textContent = 'null';
-        // [END_EXCLUDE]
-    }
-    // [START_EXCLUDE silent]
-    document.getElementById('quickstart-sign-in').disabled = false;
-    // [END_EXCLUDE]
+            document.getElementById('quickstart-sign-in').disabled = false;
     });
-    // [END authstatelistener]
     document.getElementById('quickstart-sign-in').addEventListener('click', toggleSignIn, false);
     document.getElementById('quickstart-sign-up').addEventListener('click', handleSignUp, false);
     document.getElementById('quickstart-verify-email').addEventListener('click', sendEmailVerification, false);
     document.getElementById('quickstart-password-reset').addEventListener('click', sendPasswordReset, false);
 }
-window.onload = function() {
+
+window.onload = function () {
     initApp();
 };
