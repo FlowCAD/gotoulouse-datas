aee199266c6f878c5f2b68c664e0aabd23587850
FlowCAD <florian.cadoz@gmail.com>
2018-06-07 22:07:42 +0200

    trying to force https for osm

--- scripts/map.js
+++ scripts/map.js
@@ -19,7 +19,7 @@ var mbAttr = 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap<
         'Imagery © <a href="https://mapbox.com">Mapbox</a>',
     mbUrl = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZmxvcmlhbmNhZG96IiwiYSI6ImNqMGkzN3ZzYzAwM3MzMm80MDZ6eGQ2bmwifQ.BMmvDcBnXoWT8waOnIKNBg',
     osmAttr = '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-    osmUrl = 'http://{s}.tile.osm.org/{z}/{x}/{y}.png';
+    osmUrl = 'https://tile.osm.org/{z}/{x}/{y}.png';
 
 var grayscale = L.tileLayer(mbUrl, {id: 'mapbox.light', attribution: mbAttr}),
     satellite = L.tileLayer(mbUrl, {id: 'mapbox.satellite', attribution: mbAttr}),
